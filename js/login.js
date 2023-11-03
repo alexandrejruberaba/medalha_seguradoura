8288b97e372f07ee2305de7e8f42979ae56012dc
alexandrejruberaba <alexandrejruberaba@gmail.com>
2023-11-03 01:19:28 -0300

    Inserção página de login

---
+++ js/login.js
@@ -0,0 +1,19 @@
+// login.js
+console.log("main.js está sendo executado!");
+/*
+function validarForm() {
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+
+    // Adicione aqui a lógica de validação (pode ser feita em JavaScript ou no lado do servidor)
+
+    // Exemplo simples: verifica se os campos não estão vazios
+    if (email === "" || senha === "") {
+        alert("Preencha todos os campos");
+        return false;
+    }
+
+    // Aqui você pode adicionar lógica adicional, como enviar os dados para o servidor
+   
+    return true;
+}*/
--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-// Adicione este script ao final do body ou em um arquivo JavaScript externo
-document.querySelectorAll('a.nav-item.nav-link').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+document.addEventListener('DOMContentLoaded', function () {
+    // Adicione o código de rolagem suave
+    document.querySelectorAll('a.nav-item.nav-link').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
+  
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
-
+  
         if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - 80, // Ajuste conforme a altura do cabeçalho
-                behavior: 'smooth'
-            });
+          window.scrollTo({
+            top: targetElement.offsetTop - 80, // Ajuste conforme a altura do cabeçalho
+            behavior: 'smooth'
+          });
         }
+      });
     });
-});
+  
+    // Adicione o código de redirecionamento para a página de login
+    const minhaAreaLink = document.querySelector('.nav-item.nav-link[href="./login.html"]');
+    if (minhaAreaLink) {
+      minhaAreaLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Evita o comportamento padrão do link
+        window.location.href = "./login.html"; // Redireciona para a página de login
+      });
+    }
+  });
+  
