27cae583ee3fd7b9be022e2088bbf481666f5c1a
alexandrejruberaba <alexandrejruberaba@gmail.com>
2023-11-10 15:30:53 -0300

    tabela clientes ultima versão

--- clientes/clientes.php
+++ clientes/clientes.php
@@ -39,7 +39,7 @@
                                     <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Cadastrar Clientes</button>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Editar/Buscar</button>
+                                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Editar/Excluir</button>
                                 </li>
                                 <li class="nav-item" role="presentation">
                                     <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Orçamentos</button>
--- clientes/edit_clientes.php
+++ clientes/edit_clientes.php
@@ -1,62 +1,133 @@
 <?php
-include('../data_base/conexao.php');
+require_once('../data_base/conexao.php');
+
 $mensagemErro = $mensagemSucesso = '';
+
+// Consulta SQL
 $sql = "SELECT * FROM clientes ORDER BY cpfcnpj DESC;";
 $result = $conexao->query($sql);
 
-// Lembre-se de fechar a conexão quando terminar de usá-la
+// Fechar a conexão quando terminar de usá-la
 $conexao->close();
-
 ?>
 
-<div class="container">
-    <div class="card border mx-auto mt-3 ms-3 me-3">
-        <div class="card-body">
-            <form action="salvar_cliente.php" enctype="multipart/form-data" method="post">
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap.min.css">
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.6/css/jquery.dataTables.css">
+
+    <!-- jQuery -->
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <!-- DataTables JS -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+
+
+</head>
+
+<body>
+    <div class="container">
+        <!-- Formulário de Busca -->
+        <div class="card border mx-auto mt-3 ms-3 me-3">
+            <div class="card-body">
                 <div class="row">
                     <div class="col-8">
                         <div class="mb-3">
                             <label for="cpfCnpj" class="form-label">* Informe o CPF/CNPJ:</label>
-                            <input type="text" class="form-control" id="cpfCnpj" name="cpfCnpj" placeholder="Digite apenas números" autocomplete="off">
+                            <input type="text" id="filterInput" placeholder="Filtrar por CPF/CNPJ">
                         </div>
                     </div>
-
                     <div class="col-4 d-flex align-items-center mt-2">
-                        <button class="btn btn-primary">buscar</button>
+                        <button class="btn btn-primary" onclick="buscar()">Buscar</button>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
-    </div>
 
-    <div class="card border mx-auto mt-3 ms-3 me-3">
-        <div class="card-body">
-            <div style="max-height: 300px; overflow-y: auto;">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Cpf/Cnpj</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Endereço</th>
-                            <th scope="col">Celular</th>
-                            <th scope="col">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        while ($user_data = mysqli_fetch_assoc($result)) {
-                            echo "<tr>";
-                            echo "<td>" . $user_data['cpfcnpj'] . "</td>";
-                            echo "<td>" . $user_data['nomeCompleto'] . "</td>";
-                            echo "<td>" . $user_data['logradouro'] . "</td>";
-                            echo "<td>" . $user_data['celular'] . "</td>";
-                            echo "<td>ações</td>";
-                            echo "</tr>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
+        <!-- Tabela de Dados -->
+        <div class="card border mx-auto mt-3 ms-3 me-3">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <div class="table-container">
+                        <table class="table table-striped table-hover" id="edit_clientes">
+                            <thead>
+                                <tr>
+                                    <th>Cpf/Cnpj</th>
+                                    <th>Nome</th>
+                                    <th>Endereço</th>
+                                    <th>Celular</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                while ($user_data = mysqli_fetch_assoc($result)) {
+                                    echo "<tr>";
+                                    echo "<td>" . $user_data['cpfcnpj'] . "</td>";
+                                    echo "<td>" . $user_data['nomeCompleto'] . "</td>";
+                                    echo "<td>" . $user_data['logradouro'] . "</td>";
+                                    echo "<td>" . $user_data['celular'] . "</td>";
+                                    echo "<td>ações</td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+    <script>
+        function buscar() {
+            const filterInput = document.getElementById('filterInput');
+            const filterValue = filterInput.value.toUpperCase();
+            const rows = document.querySelectorAll('tbody tr');
+
+            rows.forEach(row => {
+                const cellValue = row.cells[0].textContent || row.cells[0].innerText; // Assume que CPF/CNPJ está na primeira coluna
+                if (cellValue.toUpperCase().indexOf(filterValue) > -1) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        }
+
+        // Inicializa DataTables
+        $(document).ready(function() {
+            // Verifica se a tabela já possui DataTable antes de inicializar
+            if (!$.fn.DataTable.isDataTable('#edit_clientes')) {
+                $('#edit_clientes').DataTable({
+                    responsive: true,
+                    "language": {
+                        "lengthMenu": "Mostrar _MENU_ registros por página",
+                        "zeroRecords": "Nenhum registro encontrado",
+                        "info": "Mostrando página _PAGE_ de _PAGES_",
+                        "infoEmpty": "Nenhum registro disponível",
+                        "infoFiltered": "(filtrado de _MAX_ registros no total)",
+                        "search": "Buscar:",
+                        "paginate": {
+                            "first": "Primeiro",
+                            "last": "Último",
+                            "next": "Próximo",
+                            "previous": "Anterior"
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+        // Adiciona um evento de input ao campo de filtro
+        const filterInput = document.getElementById('filterInput');
+        filterInput.addEventListener('input', buscar);
+    </script>
+</body>
+
+</html>
